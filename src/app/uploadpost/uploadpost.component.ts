469ce0786ce3f42ff392c8d8f2cd3fb0862bc2ce
Sanchez, Lizel P <81941442+Lizel19@users.noreply.github.com>
2023-01-29 20:03:55 +0800

    Update uploadpost.component.ts

--- src/app/uploadpost/uploadpost.component.ts
+++ src/app/uploadpost/uploadpost.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-uploadpost',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./uploadpost.component.css']
 })
 export class UploadpostComponent {
+  title: string;
+  cover: File;
 
+  constructor(private http: HttpClient) {}
+
+  onTitleChanged(event: any) {
+    this.title = event.target.value;
+  }
+
+  onImageChanged(event: any) {
+    this.cover = event.target.files[0];
+  }
+
+  newBook() {
+    const uploadData = new FormData();
+    uploadData.append('title', this.title);
+    uploadData.append('cover', this.cover, this.cover.name);
+    this.http.post('http://127.0.0.1:8000/outfit/', uploadData).subscribe(
+      data => console.log(data),
+      error => console.log(error)
+    );
+  }
 }
