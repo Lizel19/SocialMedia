1dec5e38d2e73c2e5e981a55df8f52331a7990e1
Sanchez, Lizel P <81941442+Lizel19@users.noreply.github.com>
2023-01-29 20:07:23 +0800

    Update urls.py

--- backend/AestheticAPI/AestheticAPI/urls.py
+++ backend/AestheticAPI/AestheticAPI/urls.py
@@ -13,10 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+router = routers.DefaultRouter()
+router.register('outfit', OutfitViewSet)
+router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api/', include('aestheticApp.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
