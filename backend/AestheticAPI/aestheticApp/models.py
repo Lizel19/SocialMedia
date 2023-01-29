136d78f79335cea1b7735de91e1c783df495c34b
Sanchez, Lizel P <81941442+Lizel19@users.noreply.github.com>
2023-01-29 20:05:12 +0800

    Update models.py

--- backend/AestheticAPI/aestheticApp/models.py
+++ backend/AestheticAPI/aestheticApp/models.py
@@ -1,6 +1,15 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
+def upload_path(instance, filname):
+    #return '/'.join(['covers', str(instance.title), filname])
+    return '/'.join([filname])
+
+class Outfit(models.Model):
+    title = models.CharField(max_length=32, blank=False)
+    cover = models.ImageField(blank=True, null=True, upload_to=upload_path)
+
 class User(AbstractUser):
     full_name = models.CharField(max_length=255)
     email = models.CharField(max_length=255, unique=True)
